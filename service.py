7ca1e57d31f030905ea1b6b5316489dcee89b958
sina khalili <sina.khalili@canalyst.com>
2019-08-21 09:40:11 -0700

    Change port for heroku

--- service.py
+++ service.py
@@ -1,9 +1,12 @@
+import os
+
 from sanic import Sanic, request
 from sanic.response import json
 from sanic_cors import CORS, cross_origin
 
 app = Sanic(__name__)
 cors = CORS(app)
+port = os.environ['$PORT']
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -13,5 +16,5 @@ async def test_fanction(request):
     return json({'result': 'big things on the way'})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8002, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
