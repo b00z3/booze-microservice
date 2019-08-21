a3aa998d977e2840b2ac612e1f904732a5797d41
sina khalili <sina.khalili@canalyst.com>
2019-08-21 00:28:23 -0700

    Add dockerfile, service scaffolding, and requirements

---
+++ service.py
@@ -0,0 +1,17 @@
+from sanic import Sanic, request
+from sanic.response import json
+from sanic_cors import CORS, cross_origin
+
+app = Sanic(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/", methods=['GET', 'POST'])
+@cross_origin(app)
+async def test_fanction(request):
+    return json({'result': 'big things on the way'})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8002, debug=False)
+
