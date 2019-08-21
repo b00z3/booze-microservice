dd24bfec7ecf2c27342963a60be96bef0fff26a2
sina khalili <sina.khalili@canalyst.com>
2019-08-21 13:23:31 -0700

    Change default sentence

--- service.py
+++ service.py
@@ -13,7 +13,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/")
 @cross_origin(app)
 async def test_fanction(request):
-    return json({'result': 'big things on the way'})
+    return json({'result': 'big things definitely on the way'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=False)
