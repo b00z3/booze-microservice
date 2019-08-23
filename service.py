ca6d7ba5625d5f8661ebecd2facd327451f2f1d4
Parsa Habibi <parsahabibi@Parsas-MacBook-Pro.local>
2019-08-22 20:08:59 -0700

    my shit

--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 import os
 
 from sanic import Sanic, request
-from sanic.response import json
+from sanic.response import json,text,redirect
 from sanic_cors import CORS, cross_origin
 
 app = Sanic(__name__)
@@ -14,6 +14,17 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin(app)
 async def test_fanction(request):
     return json({'result': 'big things definitely on the way'})
+
+
+@app.route("/<name:string>" , methods=['GET'])
+async def funcname(request,name):
+    return json({'name' : name.format()})
+
+
+#this redirects all other traffice (i.e invalid trafic ) 
+@app.route('/<path:path>')
+async def catch_all(request , path=''):
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=False)
